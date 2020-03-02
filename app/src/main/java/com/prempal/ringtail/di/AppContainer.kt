f375527377fa9c7378e34e34e2e85f40c72c0476
Prempal Singh <prempal.singh@zomato.com>
2020-03-02 21:00:46 +0530

    add dispatcher provider to AppContainer

--- app/src/main/java/com/prempal/ringtail/di/AppContainer.kt
+++ app/src/main/java/com/prempal/ringtail/di/AppContainer.kt
@@ -1,6 +1,7 @@
 package com.prempal.ringtail.di
 
 import com.prempal.ringtail.data.ApiClient
+import com.prempal.ringtail.utils.coroutines.CoroutineDispatcherProvider
 
 /**
  * Created by prempal on 3/2/20.
@@ -9,4 +10,5 @@ class AppContainer {
 
     val apiService = ApiClient.create("https://api.github.com/repos/facebook/react-native/")
 
+    val dispatcherProvider = CoroutineDispatcherProvider()
 }
