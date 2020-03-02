29edde6396b1521ec567031fd9602fac0640f8a3
Prempal Singh <prempal.singh@zomato.com>
2020-03-02 21:14:51 +0530

    add closed PRs endpoint

--- app/src/main/java/com/prempal/ringtail/data/ApiService.kt
+++ app/src/main/java/com/prempal/ringtail/data/ApiService.kt
@@ -1,7 +1,16 @@
 package com.prempal.ringtail.data
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * Created by prempal on 3/2/20.
  */
 interface ApiService {
+
+    @GET("pulls?state=closed")
+    suspend fun getClosedPRs(
+        @Query("page") page: Int
+    ): Response<PullRequestsResponse>
 }
---
+++ app/src/main/java/com/prempal/ringtail/data/PullRequestsResponse.kt
@@ -0,0 +1,27 @@
+package com.prempal.ringtail.data
+
+import com.google.gson.annotations.SerializedName
+
+
+/**
+ * Created by prempal on 3/2/20.
+ */
+data class PullRequestsResponse(
+    @SerializedName("closed_at")
+    val closedAt: String,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("user")
+    val user: User
+)
+
+data class User(
+    @SerializedName("avatar_url")
+    val avatarUrl: String,
+    @SerializedName("login")
+    val login: String
+)
